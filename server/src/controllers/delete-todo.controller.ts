e70070fdc96f0a3d12b7db2685e357d644405d89
Ridvik Pal <ridvikpal@gmail.com>
2024-09-30 13:31:03 -0400

    Started using bruno for endpoint testing. All endpoints work properly now.

--- server/src/controllers/delete-todo.controller.ts
+++ server/src/controllers/delete-todo.controller.ts
@@ -6,10 +6,10 @@ const deleteTodoController = async (
     response: Response,
 ): Promise<void> => {
     try {
-        const deleteQuery = `DELETE FROM todo WHERE todo_id = ${request.params.id}`;
+        const deleteQuery = `DELETE FROM todo WHERE todo_id = ${request.params.id} RETURNING *`;
         const deletedTodo = await pool.query(deleteQuery);
 
-        response.status(200).send(deletedTodo);
+        response.status(200).send(deletedTodo.rows);
     } catch (error) {
         if (error instanceof Error) {
             response.status(500).send(error.message);
